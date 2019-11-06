ea4e9a63dafbab2a8b57f4665678b7d3e407ef71
shenweikang <shenweikang@daling.com>
2019-11-06 22:04:27 +0800

    🙄🙄找回样式

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,26 @@
+<!--
+ * @Author: Aaron
+ * @Date: 2019-11-06 16:36:13
+ * @LastEditors: Aaron
+ * @LastEditTime: 2019-11-06 22:03:58
+ * @Description: file content
+ -->
 <template>
   <div class="container">
     <div class="main">
-      <div class="content"></div>
-      <div class="additional"></div>
+      <div class="nav">
+        <el-input
+          v-model="search"
+          placeholder="请输入关键词并回车"
+          prefix-icon="el-icon-search"
+        />
+      </div>
+      <el-scrollbar>
+        <div class="content">
+          <Clip/>
+          <Add/>
+        </div>
+      </el-scrollbar>
     </div>
     <User />
   </div>
@@ -10,14 +28,32 @@
 
 <script>
 import User from '@/components/User.vue'
+import Clip from '@/components/Clip.vue'
+import Add from '@/components/Add.vue'
 
 export default {
   components: {
-    User
+    User,
+    Clip,
+    Add
+  },
+  data() {
+    return {
+      search: ''
+    }
   }
 }
 </script>
 
+<style lang="less">
+.nav{
+  .el-input__inner{
+    border-radius: 20px;
+    background-color: rgba(255, 255, 255, 0.2);
+    color: #fff;
+  }
+}
+</style>
 <style lang="less" scoped>
 .container{
   width: 100%;
@@ -30,15 +66,21 @@ export default {
 .main{
   display: flex;
   height: 100%;
+  flex-direction: column;
+}
+.nav{
+  display: flex;
+  justify-content: center;
+  margin: 10px;
+  .el-input{
+    width: 400px;
+  }
 }
 .content{
+  width: 100%;
+  display: flex;
   flex: 1;
-  // background-color: green;
-}
-.additional{
-  width: 240px;
-  background-color: rgba(255, 255, 255, 0.3);
-  box-shadow:0px 0 10px 0 rgba(255, 255, 255, 0.3);
-  // background-color: red;
+  flex-wrap: wrap;
+  padding: 0 70px;
 }
 </style>
